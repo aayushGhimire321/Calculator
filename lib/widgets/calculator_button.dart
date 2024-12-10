d5db696f9eb650f7f874c3c2f74cc078a79252ef
aayushghimire321 <aghimire491@gmail.com>
2024-12-10 16:14:56 +0545

    changes

--- lib/widgets/calculator_button.dart
+++ lib/widgets/calculator_button.dart
@@ -4,7 +4,7 @@ class CalculatorButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
 
-  CalculatorButton({required this.label, required this.onPressed});
+  const CalculatorButton({super.key, required this.label, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +13,7 @@ class CalculatorButton extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         backgroundColor: Colors.grey[200], // Replaces 'primary'
         foregroundColor: Colors.black, // Replaces 'onPrimary'
-        textStyle: TextStyle(fontSize: 18),
+        textStyle: const TextStyle(fontSize: 20),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8),
         ),
